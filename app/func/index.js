5b5cc534c7d6b5820210d4aae73f2a8b14499eda
LCurcuma <zhihnadya2002@gmail.com>
2026-08-16 18:55:08 +0200

    added button and generating number function

--- app/func/index.js
+++ app/func/index.js
@@ -1,8 +1,13 @@
+//global variables
 let colorsNumbers = document.getElementById("color_amount").value;
 let colorsNumbersCont = document.getElementById("color_amount");
 let colors = document.getElementById("generated_colors");
 let errorText = document.getElementById("error_text");
+let generateBTN = document.getElementById("generate");
+let colorTiles = document.getElementsByClassName("color");
+let amountOfColors = document.getElementsByClassName("color").length;
 
+//number of the colors
 colorsNumbersCont.addEventListener("input", () => {
   colorsNumbers = document.getElementById("color_amount").value;
   if (colorsNumbers > 3 && colorsNumbers <= 16) {
@@ -15,6 +20,10 @@ colorsNumbersCont.addEventListener("input", () => {
 
       color.classList.add("color");
       color.setAttribute("id", "color" + i);
+      color.setAttribute(
+        "style",
+        `background: rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()});`,
+      );
       colorLock.classList.add("fa-solid");
       colorLock.classList.add("fa-unlock");
       colorText.classList.add("color_text");
@@ -24,6 +33,8 @@ colorsNumbersCont.addEventListener("input", () => {
       color.appendChild(colorLock);
       color.appendChild(colorText);
     }
+
+    amountOfColors = document.getElementsByClassName("color").length;
   } else if (colorsNumbers <= 3) {
     errorText.innerHTML = "";
     colors.innerHTML = "";
@@ -31,6 +42,7 @@ colorsNumbersCont.addEventListener("input", () => {
     let error = document.createElement("p");
     error.innerText = "Значение не может быть меньше 4";
     errorText.appendChild(error);
+    amountOfColors = document.getElementsByClassName("color").length;
   } else {
     errorText.innerHTML = "";
     colors.innerHTML = "";
@@ -38,5 +50,37 @@ colorsNumbersCont.addEventListener("input", () => {
     let error = document.createElement("p");
     error.innerText = "Значение не может быть больше 16";
     errorText.appendChild(error);
+    amountOfColors = document.getElementsByClassName("color").length;
   }
 });
+
+//generate colors
+generateBTN.addEventListener("click", () => {
+  errorText.innerHTML = "";
+  colors.innerHTML = "";
+  for (i = 0; i < amountOfColors; i++) {
+    let color = document.createElement("div");
+    let colorLock = document.createElement("i");
+    let colorText = document.createElement("p");
+
+    color.classList.add("color");
+    color.setAttribute("id", "color" + i);
+    color.setAttribute(
+      "style",
+      `background: rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()});`,
+    );
+    colorLock.classList.add("fa-solid");
+    colorLock.classList.add("fa-unlock");
+    colorText.classList.add("color_text");
+    colorText.innerText = "#000000";
+
+    colors.appendChild(color);
+    color.appendChild(colorLock);
+    color.appendChild(colorText);
+  }
+});
+
+function getRandomColor() {
+  let number = Math.floor(Math.random() * 255);
+  return number;
+}
