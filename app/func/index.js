3a8d9fdad622f867a690f660c24532b3d1377eb5
LCurcuma <zhihnadya2002@gmail.com>
2026-08-16 17:15:06 +0200

    added the preventing logic from setting number, that less than 4

--- app/func/index.js
+++ app/func/index.js
@@ -1,27 +1,35 @@
 let colorsNumbers = document.getElementById("color_amount").value;
 let colorsNumbersCont = document.getElementById("color_amount");
 let colors = document.getElementById("generated_colors");
+let errorText = document.getElementById("error_text");
 
 colorsNumbersCont.addEventListener("input", () => {
-    colorsNumbers = document.getElementById("color_amount").value;
-    if (colorsNumbers > 0 && colorsNumbers <= 16) {
-        colors.innerHTML = "";
-        for (i = 0; i < colorsNumbers; i++){
-            let color = document.createElement("div");
-            let colorLock = document.createElement("i");
-            let colorText = document.createElement("p");
+  colorsNumbers = document.getElementById("color_amount").value;
+    if (colorsNumbers > 3 && colorsNumbers <= 16) {
+    errorText.innerHTML = "";
+    colors.innerHTML = "";
+    for (i = 0; i < colorsNumbers; i++) {
+      let color = document.createElement("div");
+      let colorLock = document.createElement("i");
+      let colorText = document.createElement("p");
 
-            color.classList.add("color");
-            color.setAttribute("id", "color" + i);
-            colorLock.classList.add("fa-solid");
-            colorLock.classList.add("fa-unlock");
-            colorText.classList.add("color_text");
-            colorText.innerText = "#000000";
+      color.classList.add("color");
+      color.setAttribute("id", "color" + i);
+      colorLock.classList.add("fa-solid");
+      colorLock.classList.add("fa-unlock");
+      colorText.classList.add("color_text");
+      colorText.innerText = "#000000";
 
-            colors.appendChild(color);
-            color.appendChild(colorLock);
-            color.appendChild(colorText);
-        }
+      colors.appendChild(color);
+      color.appendChild(colorLock);
+      color.appendChild(colorText);
     }
-    console.log(colorsNumbers);
-})
+  } else if (colorsNumbers <= 3) {
+    errorText.innerHTML = "";
+    colors.innerHTML = "";
+
+    let error = document.createElement("p");
+    error.innerText = "Значение не может быть меньше 4";
+    errorText.appendChild(error);
+  }
+});
