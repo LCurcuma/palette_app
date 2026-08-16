21f8d5749f421458ba21c4415113111abf1e771e
LCurcuma <zhihnadya2002@gmail.com>
2026-08-16 16:47:36 +0200

    added the function for changing the number of colors

--- app/func/index.js
+++ app/func/index.js
@@ -1,8 +1,27 @@
 let colorsNumbers = document.getElementById("color_amount").value;
 let colorsNumbersCont = document.getElementById("color_amount");
+let colors = document.getElementById("generated_colors");
 
 colorsNumbersCont.addEventListener("input", () => {
     colorsNumbers = document.getElementById("color_amount").value;
+    if (colorsNumbers > 0 && colorsNumbers <= 16) {
+        colors.innerHTML = "";
+        for (i = 0; i < colorsNumbers; i++){
+            let color = document.createElement("div");
+            let colorLock = document.createElement("i");
+            let colorText = document.createElement("p");
+
+            color.classList.add("color");
+            color.setAttribute("id", "color" + i);
+            colorLock.classList.add("fa-solid");
+            colorLock.classList.add("fa-unlock");
+            colorText.classList.add("color_text");
+            colorText.innerText = "#000000";
+
+            colors.appendChild(color);
+            color.appendChild(colorLock);
+            color.appendChild(colorText);
+        }
+    }
     console.log(colorsNumbers);
 })
-console.log(colorsNumbersCont);
